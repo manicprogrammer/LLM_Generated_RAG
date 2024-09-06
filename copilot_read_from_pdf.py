8a6bdc63cb595aee5afbf3724787e821090bde0c
Michael Ruminer <michael.ruminer@gmail.com>
2024-09-06 16:55:43 -0400

    Update copilot_read_from_pdf.py to explain with more detail why there is no output

--- copilot_read_from_pdf.py
+++ copilot_read_from_pdf.py
@@ -1,2 +1,4 @@
 # despite having the flag to allow for use of public code copilot is blocking the request because it is using public code
+# the results were the following message: "Sorry, the response matched public code so it was blocked. Please rephrase your prompt. Learn more." 
+# For more info see: https://m-ruminer.medium.com/having-ai-tools-generate-code-for-ai-tools-9563e0ab3deb
 # Simple Prompt: provide me python code to use OpenAI API to perform retrieval augmented generation on a pdf
